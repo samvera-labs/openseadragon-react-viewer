b44258761be7b4e8be48ccd5613af6404557d860
Adam Arling <adam.arling@northwestern.edu>
2020-01-22 13:25:56 -0600

    Update webpack configuration to ignore React dependencies in the build

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,9 @@ module.exports = {
   output: {
     path: path.resolve("lib"),
     filename: "OpenSeadragonViewer.js",
-    libraryTarget: "commonjs2"
+    libraryTarget: "umd",
+    publicPath: "/lib/",
+    umdNamedDefine: true
   },
   module: {
     rules: [
@@ -29,5 +31,26 @@ module.exports = {
         ]
       }
     ]
+  },
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, "./node_modules/react"),
+      "react-dom": path.resolve(__dirname, "./node_modules/react-dom")
+    }
+  },
+  externals: {
+    // Don't bundle react or react-dom
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React"
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "ReactDOM",
+      root: "ReactDOM"
+    }
   }
 };
